3cbb58d53704efbcbcbe2498d37325e22c9b3d03
Max Glass <glassmax13@gmail.com>
2022-03-23 12:55:08 -0400

    Handle loading better in ArticlePage

--- src/pages/articlePage.tsx
+++ src/pages/articlePage.tsx
@@ -4,12 +4,13 @@ import { getArticleBySlug } from "../api/requests";
 import { IArticle, IApiError } from "../types";
 import { VariableContainer } from "../components/variableContainer";
 import styled from "styled-components";
-import { Row } from "react-bootstrap";
+import { Placeholder, Row } from "react-bootstrap";
 import { HorizontalDivider } from "../components/horizontalDivider";
 import { ThemedLink } from "../components/themedLink";
 import { TimeStamp } from "../components/timeStamp";
 import { HeadlineFont } from "../components/headlineFont";
 import { useQuery } from "react-query";
+import { TextPlaceholder } from "../components/textPlaceholder";
 
 const StyledContainer = styled(VariableContainer)`
   margin-top: 50px;
@@ -28,53 +29,94 @@ export const ArticlePage = () => {
 
   const {
     data: article,
-    isSuccess: articleLoaded,
-    isLoading: articleLoading,
-    isError: isArticleError,
-    error: articleError,
+    isIdle,
+    isLoading,
+    isError,
+    error,
   } = useQuery<IArticle, IApiError, IArticle>(["articles", slug!], () =>
     getArticleBySlug(slug!),
   );
 
-  if (isArticleError) {
-    return <p>Error loading article: {articleError.message}</p>;
+  if (isError) {
+    return <p>Error loading article: {error.message}</p>;
   }
-
-  if (articleLoading || !articleLoaded) {
-    return <p>Loading...</p>;
-  }
-
-  const writerUrl = `/writer/${article.writer.firstName}-${article.writer.lastName}`;
 
   return (
     <>
       <NavigationBar />
       <StyledContainer fluid="lg" xl={1066}>
         <BlackDivider />
-        <Row>
-          <p className="text-uppercase" style={{ fontSize: "0.8rem" }}>
-            <ThemedLink to={article.section.permalink}>
-              {article.section.name}
-            </ThemedLink>
-          </p>
-          <HeadlineFont>
-            <h1 className="text-center fw-bold">{article.headline}</h1>
-          </HeadlineFont>
-          <p className="lh-1">
-            By{" "}
-            <ThemedLink className="fw-bold" to={writerUrl}>
-              {article.writer.firstName} {article.writer.lastName}
-            </ThemedLink>
-          </p>
-          <p className="text-muted">
-            <TimeStamp originalDate={article.publicationDate} />
-          </p>
-        </Row>
-        <StyledDivider />
-        <Row style={{ fontFamily: "georgia", fontSize: "1.25rem" }}>
-          {article.body}
-        </Row>
+        {isLoading || isIdle ? (
+          <ArticlePagePlaceholder />
+        ) : (
+          <>
+            <Row>
+              <p className="text-uppercase" style={{ fontSize: "0.8rem" }}>
+                <ThemedLink to={article.section.permalink}>
+                  {article.section.name}
+                </ThemedLink>
+              </p>
+              <HeadlineFont>
+                <h1 className="text-center fw-bold">{article.headline}</h1>
+              </HeadlineFont>
+              <p className="lh-1">
+                By{" "}
+                <ThemedLink
+                  className="fw-bold"
+                  to={`/writer/${article.writer.firstName}-${article.writer.lastName}`}>
+                  {article.writer.firstName} {article.writer.lastName}
+                </ThemedLink>
+              </p>
+              <p className="text-muted">
+                <TimeStamp originalDate={article.publicationDate} />
+              </p>
+            </Row>
+            <StyledDivider />
+            <Row style={{ fontFamily: "georgia", fontSize: "1.25rem" }}>
+              {article.body}
+            </Row>
+          </>
+        )}
       </StyledContainer>
+    </>
+  );
+};
+
+const ArticlePagePlaceholder = () => {
+  return (
+    <>
+      <Row>
+        <Placeholder as="p" animation="glow" size="sm">
+          <TextPlaceholder xs={1} />
+        </Placeholder>
+        <Placeholder as="h1" animation="glow" className="text-center" size="lg">
+          <TextPlaceholder xs={8} />
+        </Placeholder>
+        <Placeholder as="p" animation="glow">
+          <TextPlaceholder xs={1} />
+          <br />
+          <TextPlaceholder xs={2} />
+        </Placeholder>
+      </Row>
+      <StyledDivider />
+      <Row style={{ fontSize: "1.25rem" }}>
+        {Array.from(Array(6).keys()).map(idx => (
+          <Placeholder as="p" animation="glow" key={idx}>
+            {Array.from(Array(Math.floor(Math.random() * 4) + 2).keys()).map(
+              (idx, _, { length }) => (
+                <TextPlaceholder
+                  key={idx}
+                  xs={
+                    idx !== length - 1
+                      ? Math.floor(Math.random() * 3) + 10
+                      : Math.floor(Math.random() * 4) + 1
+                  }
+                />
+              ),
+            )}
+          </Placeholder>
+        ))}
+      </Row>
     </>
   );
 };

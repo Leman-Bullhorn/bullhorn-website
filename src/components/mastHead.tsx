40d3c4d9e10d7014ccf06b1943e30a06757cc7c5
Max Glass <glassmax13@gmail.com>
2022-11-16 16:13:50 -0500

    Make masthead look better

--- src/components/mastHead.tsx
+++ src/components/mastHead.tsx
@@ -4,14 +4,52 @@ import styled from "styled-components";
 import { LinkContainer } from "./linkContainer";
 import { sections } from "../types";
 import { useInView } from "react-intersection-observer";
+import { HeadlineFont } from "./headlineFont";
 
-const NavItem = styled(Nav.Item)`
-  :last-child {
-    border-right: none;
+const StyledNavbar = styled(Navbar)`
+  .navbar-brand {
+    padding: 0;
+    margin: 0;
+    font-family: Amador;
+    font-size: 100px;
+    line-height: 80px;
+    -webkit-text-stroke: 2.5px rgb(168, 195, 217);
   }
-  /* margin-top: 0.25rem;
-  margin-bottom: 0.25rem; */
-  border-right: 1px solid;
+
+  .navbar-text {
+    color: rgb(${({ theme }) => theme.lemanColorComponents});
+  }
+`;
+
+const BrandText = styled.a`
+  color: rgb(${({ theme }) => theme.lemanColorComponents});
+  text-decoration-line: none;
+  line-height: 1rem;
+  font-weight: 500;
+  transition: color 0.25s ease-in-out, background-color 0.25s ease-in-out,
+    border-color 0.25s ease-in-out;
+
+  :hover {
+    text-decoration: underline;
+  }
+`;
+
+const StyledLink = styled(Nav.Link)`
+  color: black;
+  text-decoration-color: black;
+  text-decoration-line: none;
+  font-size: 1.15rem;
+  line-height: 1rem;
+  font-weight: 500;
+  transition: color 0.25s ease-in-out, background-color 0.25s ease-in-out,
+    border-color 0.25s ease-in-out;
+
+  :hover,
+  :focus {
+    color: black;
+    background-color: rgba(${({ theme }) => theme.lemanColorComponents}, 0.5);
+    text-decoration: underline;
+  }
 `;
 
 interface MastHeadProps {
@@ -46,7 +84,7 @@ export const Masthead = (props: MastHeadProps) => {
           href="https://www.lemanmanhattan.org/"
           target="_blank"
           rel="noreferrer">
-          Léman Manhattan Preparatory School
+          <HeadlineFont>Léman Manhattan Preparatory School</HeadlineFont>
         </BrandText>
       </Row>
       <Row style={{ boxShadow: "0px 5px 5px -5px rgba(0, 0, 0, 0.28)" }}>
@@ -69,64 +107,3 @@ export const Masthead = (props: MastHeadProps) => {
     </Container>
   );
 };
-
-const StyledNavbar = styled(Navbar)`
-  .navbar-brand {
-    padding: 0;
-    margin: 0;
-    font-family: Amador;
-    font-size: 100px;
-    line-height: 80px;
-    -webkit-text-stroke: 2.5px rgb(168, 195, 217);
-  }
-
-  .navbar-text {
-    color: rgb(${({ theme }) => theme.lemanColorComponents});
-  }
-`;
-
-// const BorderedRow = styled(Row)`
-//   padding: 0 60px;
-//   padding-bottom: 0.25rem;
-//   border-bottom: 2px solid black;
-//   margin-bottom: 0.5rem;
-
-//   :before {
-//     content: "";
-//     height: 1px;
-//     width: 100%;
-//     border-top: 1px solid black;
-//     padding-top: 0.25rem;
-//   }
-// `;
-
-const BrandText = styled.a`
-  color: rgb(${({ theme }) => theme.lemanColorComponents});
-  text-decoration-line: none;
-  line-height: 1rem;
-  font-weight: 500;
-  transition: color 0.25s ease-in-out, background-color 0.25s ease-in-out,
-    border-color 0.25s ease-in-out;
-
-  :hover {
-    text-decoration: underline;
-  }
-`;
-
-const StyledLink = styled(Nav.Link)`
-  color: black;
-  text-decoration-color: black;
-  text-decoration-line: none;
-  font-size: 1.15rem;
-  line-height: 1rem;
-  font-weight: 500;
-  transition: color 0.25s ease-in-out, background-color 0.25s ease-in-out,
-    border-color 0.25s ease-in-out;
-
-  :hover,
-  :focus {
-    color: black;
-    background-color: rgba(${({ theme }) => theme.lemanColorComponents}, 0.5);
-    text-decoration: underline;
-  }
-`;

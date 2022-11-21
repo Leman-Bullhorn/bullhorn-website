25be7be3cd6ac2b3e0da89c299907682fdecae82
Max Glass <glassmax13@gmail.com>
2022-11-20 23:01:49 -0500

    Make featured article wrap text around image
    Need to fix the issue of images with less height leaving large
    whitepace, this is just a hacky impl for now.

--- src/components/featuredArticle.tsx
+++ src/components/featuredArticle.tsx
@@ -1,4 +1,4 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { IArticle } from "../types";
@@ -6,18 +6,10 @@ import { HeadlineFont } from "./headlineFont";
 import { ThemedLink } from "./themedLink";
 import { TimeStamp } from "./timeStamp";
 
-const StyledFigure = styled.figure`
-  max-height: 480px;
-  overflow: hidden;
-
-  img {
-    width: 100%;
-  }
-`;
-
 const ArticleContainer = styled(Container)`
   padding-bottom: 1rem;
   border-bottom: 1px solid #dddddd;
+  min-height: 500px;
 `;
 
 export const FeaturedArticle: React.FC<{ article: IArticle }> = ({
@@ -28,40 +20,39 @@ export const FeaturedArticle: React.FC<{ article: IArticle }> = ({
 
   return (
     <ArticleContainer fluid>
-      <Row>
-        <Col xs={4}>
+      <Col>
+        {article.imageUrl && (
+          <Link to={articleUrl}>
+            <img
+              src={article.imageUrl}
+              alt=""
+              width="66.667%"
+              style={{ float: "right", maxHeight: "480px", overflow: "hidden" }}
+            />
+          </Link>
+        )}
+
+        <div>
           <HeadlineFont>
             <ThemedLink to={articleUrl}>
-              <h3 className="d-inline-block">{article.headline}</h3>
+              <h4 style={{ display: "flow-root" }}>{article.headline}</h4>
             </ThemedLink>
           </HeadlineFont>
 
-          <p className="display-inline-black text-muted mb-2 fs-6">
-            {article.focus}
-          </p>
+          <p className="text-muted my-2 fs-6">{article.focus}</p>
+        </div>
 
-          <span>
-            By{" "}
-            <ThemedLink to={writerUrl}>
-              {article.writer.firstName} {article.writer.lastName}
-            </ThemedLink>
-          </span>
+        <span>
+          By{" "}
+          <ThemedLink to={writerUrl}>
+            {article.writer.firstName} {article.writer.lastName}
+          </ThemedLink>
+        </span>
 
-          <p className="text-muted">
-            <TimeStamp originalDate={article.publicationDate} />
-          </p>
-        </Col>
-
-        <Col xs={8}>
-          {article.imageUrl && (
-            <Link to={articleUrl}>
-              <StyledFigure>
-                <img src={article.imageUrl} alt="" />
-              </StyledFigure>
-            </Link>
-          )}
-        </Col>
-      </Row>
+        <p className="text-muted mb-0">
+          <TimeStamp originalDate={article.publicationDate} />
+        </p>
+      </Col>
     </ArticleContainer>
   );
 };

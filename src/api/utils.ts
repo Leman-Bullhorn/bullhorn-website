996aac218e1330f7681c1d8ddf681a7a36f8214a
Max Glass <glassmax13@gmail.com>
2022-11-21 02:14:14 -0500

    Fix incorrect base url

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -1,12 +1,10 @@
 import Axios, { AxiosResponse } from "axios";
 import applyCaseMiddleware from "axios-case-converter";
 
-// export const BASE_URL =
-//   process.env.NODE_ENV === "production"
-//     ? `https://${window.location.host}/api`
-//     : "http://localhost:8000/api";
-
-export const BASE_URL = "http://localhost:8000/api";
+export const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? `https://${window.location.host}/api`
+    : "http://localhost:8000/api";
 
 export const axios = applyCaseMiddleware(
   Axios.create({
